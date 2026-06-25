526d165a12594745a5fe53cf4c08cfbc97a69833
surajguvaju079 <surajguvaju.079@kathford.edu.nnp>
2026-06-25 07:20:26 +0000

    predicted with 0.8 accuracy

--- load.py
+++ load.py
@@ -1,31 +1,25 @@
-
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
 model = LinearRegression()
-df = pd.read_csv("earnings.csv")
+df = pd.read_csv("loads.csv");
+X=df[["loads_completed"]]
+y=df["earning"]
 
-print(df)
-
-X = df[["distance_km"]];
-y = df["earning"];
-
-X_train,X_test,y_train,y_test = train_test_split(
-    X,y,test_size=0.2,random_state=42
-)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42);
 
 model.fit(X_train,y_train)
 prediction = model.predict(X_test);
 score = r2_score(y_test,prediction)
-print(prediction)
+print(model.predict([[12]]))
+print(model.predict([[14]]))
+print(score)
 print(model.coef_)
 print(model.intercept_)
-print(score)
-result = pd.DataFrame({
-    "actual":y_test,
-    "predicted":prediction
-})
-print(result)
+
+
+
+
 
